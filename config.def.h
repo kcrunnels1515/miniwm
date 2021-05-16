17b0743b8342ca2e3c9b00286b1b5d520af4ad2f
KCRunnels15 <kcrunnels15@gmail.com>
2021-05-16 09:56:38 -0400

    Small oops

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 static const char col_purple[]	    = "#4c2172";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
