792a7cffb2822ddd6c6e53705a435220f609abaf
Kelly Runnels <kellyr@debbox.my.domain>
2021-06-17 08:22:02 -0400

    Initial commit

--- config.def.h
+++ config.def.h
@@ -3,6 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack-Regular:size=10" };
@@ -23,6 +27,8 @@ static const char *const autostart[] = {
 	"nitrogen", "--restore", NULL,
 	"xset", "r", "rate", "300", "70", NULL,
 	"setxkbmap", "-option", "caps:swapescape", NULL,
+	"volumeicon", NULL,
+	"nm-applet", NULL,
 	/* "xmodmap", "-e", "'keycode 135 = Multi_key'", NULL, */
 	NULL /* terminate */
 };
